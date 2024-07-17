c5265f429d3b6e4a55907692587b9993fbe2f87e
Cristian Hubati <cristihubati03@gmail.com>
2024-07-17 23:06:12 +0300

    Single Linked Lists Not Solved

---
+++ LeetCode/Singly Linked Lists/Reverse Linked List.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Singly_Linked_Lists;
+
+// Link: https://leetcode.com/problems/reverse-linked-list/description/
+public class Reverse_Linked_List
+{
+    static void Main(string[] args)
+    {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        Solution solution = new Solution();
+        ListNode reversedList = solution.ReverseList(head);
+
+        while(reversedList is not null)
+        {
+            Console.WriteLine(reversedList.val);
+            reversedList = reversedList.next;
+        }
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        
+    }
+}

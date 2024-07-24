14b2c138e3343ff1764ed0e491c84068ce86bffb
Cristian Hubati <cristihubati03@gmail.com>
2024-07-24 23:09:04 +0300

    Queues

---
+++ LeetCode/Queues/Implement Stack using Queues.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode.Queues;
+
+// Link: https://leetcode.com/problems/implement-stack-using-queues/description/
+public class Implement_Stack_using_Queues
+{
+    static void Main(string[] args)
+    {
+        // Example 1
+        MyStack example1 = new();
+        example1.Push(1);
+        example1.Push(2);
+        Console.WriteLine(example1.Top());
+        Console.WriteLine(example1.Pop());
+        Console.WriteLine(example1.Empty());
+    }
+
+    private class MyStack
+    {
+        public Queue<int> Queue { get; set; }
+        public Queue<int> Reversed { get; set; }
+
+        public MyStack()
+        {
+            Queue = new Queue<int>();
+            Reversed = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            Queue.Enqueue(x);
+            Reversed = ReverseQueue(Queue);
+        }
+
+        public int Pop()
+        {
+            int popValue = Reversed.Dequeue();
+            Queue = ReverseQueue(Reversed);
+                
+            return popValue;
+        }
+
+        public int Top()
+        {
+            return Reversed.Peek();
+        }
+
+        public bool Empty()
+        {
+            if (Queue.Count == 0)
+                return true;
+
+            return false;
+        }
+
+        private static Queue<int> ReverseQueue(Queue<int> normal)
+        {
+            Queue<int> reversed = new Queue<int>();
+
+            IEnumerable<int> reversedItems = normal.Reverse();
+
+            foreach (int x in reversedItems)
+            {
+                reversed.Enqueue(x);
+            }
+
+            return reversed;
+        }
+    }
+}
---
+++ LeetCode/Queues/Number of Students Unable to Eat Lunch.cs
@@ -0,0 +1,81 @@
+﻿namespace LeetCode.Queues;
+
+// Link: https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/
+public class Number_of_Students_Unable_to_Eat_Lunch
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new();
+
+        // Example 1
+        int[] students1 = [1, 1, 0, 0];
+        int[] sandwiches1 = [0, 1, 0, 1];
+        Console.WriteLine(solution.CountStudents(students1, sandwiches1));
+
+        // Example 2
+        int[] students2 = [1, 1, 1, 0, 0, 1];
+        int[] sandwiches2 = [1, 0, 0, 0, 1, 1];
+        Console.WriteLine(solution.CountStudents(students2, sandwiches2));
+
+        // Example 3
+        int[] students3 = [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1];
+        int[] sandwiches3 = [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0];
+        Console.WriteLine(solution.CountStudents(students3, sandwiches3));
+    }
+
+    private class Solution
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            int index = 0;
+
+            int sandwichesLength = sandwiches.Length;
+            int studentsLength = students.Length;
+
+            while (sandwichesLength > 0 && studentsLength > 0)
+            {
+                int firstSandwich = sandwiches[0];
+                if (!ContainsElementWithinLength(students, studentsLength, firstSandwich))
+                {
+                    break;
+                }
+
+                if (sandwiches[index] == students[index])
+                {
+                    RemoveFirstElement(students);
+                    RemoveFirstElement(sandwiches);
+
+                    studentsLength--;
+                    sandwichesLength--;
+                }
+                else
+                {
+                    int value = students[index];
+                    RemoveFirstElement(students);
+                    students[studentsLength - 1] = value;
+                }
+            }
+
+            return studentsLength;
+        }
+
+        private void RemoveFirstElement(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+        }
+
+        private bool ContainsElementWithinLength(int[] array, int length, int element)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i] == element)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
